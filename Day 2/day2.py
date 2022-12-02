571f5ae57392bd25e46884b572146a62ad2246e0
chillymosh <86857777+chillymosh@users.noreply.github.com>
2022-12-02 11:07:05 +0000

    Update day2.py

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-with open("AdventOfCode2022/Day2/input.txt", "r") as f:
+with open("AdventOfCode2022/day2/input.txt", "r") as f:
     data = [line.strip().split(" ") for line in f]
 
 
@@ -82,6 +82,6 @@ def part2a(data: list[list[str]]) -> int:
 
 # For some unreadable ord nonsense
 p=print
-d=[(ord((c:=m.split(" "))[0])-65,ord(c[1][0])-88) for m in open("AdventOfCode2022/Day2/input.txt", "r").readlines()]
+d=[(ord((c:=m.split(" "))[0])-65,ord(c[1][0])-88) for m in open("AdventOfCode2022/day2/input.txt", "r").readlines()]
 p(sum(1+b+3*((b-a+1)%3) for a,b in d))
 p(sum(3*b+(1+(a+b+2)%3) for a,b in d))
