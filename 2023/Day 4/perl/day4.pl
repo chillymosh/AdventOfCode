715e3b3a08df16868d9a054c4f924d9d728ad39e
chillymosh <86857777+chillymosh@users.noreply.github.com>
2023-12-04 12:47:27 +0000

    Update day4.pl

--- 2023/Day 4/perl/day4.pl
+++ 2023/Day 4/perl/day4.pl
@@ -2,7 +2,6 @@
 
 use strict;
 use warnings;
-use List::Util qw(min);
 
 open my $fh, '<', 'input.txt' or die "Failed to open input.txt: $!";
 my @lines = <$fh>;
@@ -24,31 +23,38 @@ foreach my $i (0 .. $#lines) {
     };
 }
 
-
 sub calculateTotalPoints {
     my ($cards, $matches) = @_;
     my $total = 0;
-    foreach my $i (keys %$matches) {
-        if ($matches->{$i} > 0) {
-            $total += 2 ** ($matches->{$i} - 1);
-        }
+    foreach my $match (values %$matches) {
+        $total += 2 ** ($match - 1) if $match > 0;
     }
     return $total;
 }
 
+sub calculateTotalScratchcards {
+    my ($cards, $matches) = @_;
+    my @instances = (1) x (scalar keys %$cards);
+    foreach my $i (sort { $a <=> $b } keys %$matches) {
+        for (my $j = $i + 1; $j <= $i + $matches->{$i} && $j <= scalar keys %$cards; $j++) {
+            $instances[$j] += $instances[$i];
+        }
+    }
+    my $sum = 0;
+    $sum += $_ for @instances;
+    return $sum;
+}
 
 my %matches;
 foreach my $i (keys %cards) {
-    my @winning = @{$cards{$i}->{'winning'}};
-    my @mine = @{$cards{$i}->{'mine'}};
-    my %intersection;
-    @intersection{@winning} = ();
-    my @common = grep { exists $intersection{$_} } @mine;
-    $matches{$i} = scalar(@common);
+    my %count;
+    $count{$_}++ for @{$cards{$i}{winning}};
+    $count{$_}++ for @{$cards{$i}{mine}};
+    $matches{$i} = grep { $count{$_} == 2 } keys %count;
 }
 
 my $p1 = calculateTotalPoints(\%cards, \%matches);
 print "$p1\n";
 
-
-
+my $p2 = calculateTotalScratchcards(\%cards, \%matches);
+print "\n$p2\n";
