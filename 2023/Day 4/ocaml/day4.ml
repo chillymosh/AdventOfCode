3d155d70dd8a0ddd54ba8580423c057bbe671c46
chillymosh <86857777+chillymosh@users.noreply.github.com>
2023-12-04 12:51:37 +0000

    Update day4.ml

--- 2023/Day 4/ocaml/day4.ml
+++ 2023/Day 4/ocaml/day4.ml
@@ -19,11 +19,23 @@ let read_lines filename =
 
 let calculate_total_points cards =
   List.fold cards ~init:0 ~f:(fun acc (winning, mine) ->
-    let matches = List.length (List.filter winning ~f:(List.mem mine ~equal:Int.equal)) in
-    if matches > 0 then acc + (1 lsl (matches - 1)) else acc
-  )
+      let matches = List.length (List.filter winning ~f:(List.mem mine ~equal:Int.equal)) in
+      if matches > 0 then acc + (1 lsl (matches - 1)) else acc
+    )
+let calculate_total_scratchcards cards =
+  let n = List.length cards in
+  let instances = Array.create ~len:n 1 in
+  let matches = List.map cards ~f:(fun (winning, mine) ->
+      List.length (List.filter winning ~f:(List.mem mine ~equal:Int.equal))
+    ) in
+  List.iteri matches ~f:(fun i match_count ->
+      for j = i + 1 to min (i + match_count) (n - 1) do
+        instances.(j) <- instances.(j) + instances.(i)
+      done
+    );
+  Array.fold instances ~init:0 ~f:(+)
 
-  let () =
+let () =
   let argv = Sys.get_argv () in
   if Array.length argv < 2 then (
     Printf.eprintf "Usage: %s <filename>\n" argv.(0);
@@ -33,5 +45,5 @@ let calculate_total_points cards =
   let cards = read_lines filename in
   let total_points = calculate_total_points cards in
   Printf.printf "Total Points: %d\n" total_points;
-
-
+  let total_scratchcards = calculate_total_scratchcards cards in
+  Printf.printf "Total Scratchcards: %d\n" total_scratchcards
