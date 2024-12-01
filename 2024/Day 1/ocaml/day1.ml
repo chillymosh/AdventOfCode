0d0b61f0174b832c8afc8f98c2e90f7236fb14df
Chillymosh <chillymosh@gmail.com>
2024-12-01 09:22:51 +0000

    Update day1.ml

--- 2024/Day 1/ocaml/day1.ml
+++ 2024/Day 1/ocaml/day1.ml
@@ -1,29 +1,13 @@
-let preprocess_line line =
-  line
-  |> String.split_on_char '\t'
-  |> String.concat " "
-  |> String.trim
-
 let read_input file_path =
   let ic = open_in file_path in
   let rec parse acc_left acc_right =
     match input_line ic with
-    | exception End_of_file -> close_in ic; (List.rev acc_left, List.rev acc_right)
+    | exception End_of_file -> close_in ic; (acc_left, acc_right)
     | line when String.trim line = "" -> parse acc_left acc_right 
     | line ->
-      let parts =
-        preprocess_line line
-        |> String.split_on_char ' '
-        |> List.filter (fun s -> s <> "") 
-      in
-      match parts with
-      | [l; r] ->
-        (try
-           let l = int_of_string l in
-           let r = int_of_string r in
-           parse (l :: acc_left) (r :: acc_right)
-         with Failure _ ->
-           failwith ("Invalid number in line: " ^ line))
+      let parts = String.split_on_char ' ' line |> List.concat_map (String.split_on_char '\t') in
+      match List.filter (fun s -> s <> "") parts with
+      | [l; r] -> (try parse ((int_of_string l) :: acc_left) ((int_of_string r) :: acc_right) with Failure _ -> failwith ("Invalid number in line: " ^ line))
       | _ -> failwith ("Invalid line format: " ^ line)
   in
   parse [] []
@@ -34,20 +18,15 @@ let calculate_p1 left right =
   List.fold_left2 (fun acc l r -> acc + abs (l - r)) 0 sorted_left sorted_right
 
 let calculate_p2 left right =
-  let freq_table = Hashtbl.create (List.length right) in
-  List.iter (fun r ->
-      let count = try Hashtbl.find freq_table r with Not_found -> 0 in
-      Hashtbl.replace freq_table r (count + 1)
-    ) right;
-  List.fold_left (fun acc l ->
-      let count = try Hashtbl.find freq_table l with Not_found -> 0 in
-      acc + (l * count)
-    ) 0 left
+  let freq_table = List.fold_left (fun acc r ->
+      let count = try Hashtbl.find acc r with Not_found -> 0 in
+      Hashtbl.replace acc r (count + 1);
+      acc
+    ) (Hashtbl.create (List.length right)) right in
+  List.fold_left (fun acc l -> acc + (l * (try Hashtbl.find freq_table l with Not_found -> 0))) 0 left
 
 let () =
   let file_path = "../input.txt" in
   let left, right = read_input file_path in
-  let p1 = calculate_p1 left right in
-  Printf.printf "%d\n" p1;
-  let p2 = calculate_p2 left right in
-  Printf.printf "%d\n" p2
+  Printf.printf "p1: %d\n" (calculate_p1 left right);
+  Printf.printf "p2: %d\n" (calculate_p2 left right)
